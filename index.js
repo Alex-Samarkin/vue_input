0545d2bf922e6ae2d8aed23779e6583be2c0017d
alex <alex_samarkin@mail.ru>
2019-08-05 01:09:58 +0300

    2

--- index.js
+++ index.js
@@ -1,6 +1,57 @@
+let data1;
+fetch('data.json')
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (myJson) {
+    data1 = myJson;
+    console.log(JSON.stringify(myJson));
+  });
+
 var app = new Vue({
   el: "#app",
-  data: {
-    message: "Здравствуйте, Александр Иванович! Как у вас дела?"
+  data: function () {
+    return {
+      message: "Здравствуйте, Александр Иванович! Как у вас дела?",
+      jsn: [{
+          "id": 1,
+          "f_name": "Иванов",
+          "i_name": "Иван",
+          "o_name": "Иванович",
+          "Y": 1970,
+          "address": {
+            "state": "Россия",
+            "region": "Севверо-Западный",
+            "city": "Псков",
+            "street": "Грибоедова",
+            "house": "12/а",
+            "room": "34"
+          },
+          "phones": [
+            "8-921-123-12-12",
+            "1"
+          ]
+        },
+        {
+          "id": 1,
+          "f_name": "Иванов",
+          "i_name": "Иван",
+          "o_name": "Иванович",
+          "Y": 1970,
+          "address": {
+            "state": "Россия",
+            "region": "Севверо-Западный",
+            "city": "Псков",
+            "street": "Грибоедова",
+            "house": "12/а",
+            "room": "34"
+          },
+          "phones": [
+            "8-921-123-12-12",
+            "1"
+          ]
+        }
+      ]
+    };
   }
 });
