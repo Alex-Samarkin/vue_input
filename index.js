4e08893aa7324520ec4b26433c78d90dd3ccf99f
alex <alex_samarkin@mail.ru>
2019-08-06 21:56:10 +0300

    2

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-let data1;
-fetch('data.json')
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (myJson) {
-    data1 = myJson;
-    console.log(JSON.stringify(myJson));
-  });
-
 var app = new Vue({
   el: "#app",
   data: function () {
@@ -29,26 +19,26 @@ var app = new Vue({
           },
           "phones": [
             "8-921-123-12-12",
-            "1"
+            ""
           ]
         },
         {
-          "id": 1,
-          "f_name": "Иванов",
-          "i_name": "Иван",
-          "o_name": "Иванович",
-          "Y": 1970,
+          "id": 2,
+          "f_name": "Антонов",
+          "i_name": "Никанор",
+          "o_name": "Феоктистович",
+          "Y": 1952,
           "address": {
             "state": "Россия",
             "region": "Севверо-Западный",
             "city": "Псков",
-            "street": "Грибоедова",
-            "house": "12/а",
-            "room": "34"
+            "street": "Леона Поземского",
+            "house": "57",
+            "room": "11"
           },
           "phones": [
-            "8-921-123-12-12",
-            "1"
+            "8-921-123-35-42",
+            ""
           ]
         }
       ]
